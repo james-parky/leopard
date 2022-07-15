b79e3eea99af91090887ad5ad4708a5ba997f71a
jame-parky <72101291+james-parky@users.noreply.github.com>
2022-07-15 02:13:42 +0100

    Cleaned up memory leaks caused from overallocating memory in new_variable() function.

--- src/variable.c
+++ src/variable.c
@@ -7,16 +7,10 @@
 variable* new_variable(variable_type type, char* name){
     variable* var = malloc(sizeof(variable));
     if(var == NULL) printf("not allocated\n");
-    // var->type = type;
-    // printf("%zu\n",len);
-    // var->name = name;
-    // memcpy(var->values, values, sizeof(value*) * len+1);
-    // var->values[len]=NULL;
     var->type = type;
     var->name = name;
     var->value_buf_index = 0;
     var->value_count = 0;
-    for(size_t i=0;i<BUF_MAX;i++) var->value_buf[i] = malloc(sizeof(value*));
     return var;
 }
 
