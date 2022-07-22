1a80a257a14d59c0a5776a2a1703163079e9f06c
jame-parky <72101291+james-parky@users.noreply.github.com>
2022-07-22 17:47:57 +0100

    Added better command line argument handling to main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lexer.h"
+#include <string.h>
+
 #include "transpiler.h"
 
+int main(int argc, char* argv[]) {
+    if(argc < 3) fprintf(stderr, "Too few arguments supplied.\n");
+    if(argc > 3) fprintf(stderr, "Too many arguments supplied.\n");
 
-int main(int argc, char* argv[]){
-    if(argc < 2) {
-        fprintf(stderr, "Too few arguements supplied.\n");
-        exit(1);
-    }
-    char* file_name = argv[1];
-    FILE* file = fopen(file_name, "r");
-    if(!file) {
-        fprintf(stderr, "Specified file could not be opened.\n");
-        exit(1);
-    }
-    transpile_file(file);
+    char* in_file_name = argv[1];
+    char* out_file_name = argv[2];
+
+    FILE* in = fopen(in_file_name, "r");
+    FILE* out = fopen(out_file_name, "w+");
+
+    if(!in) fprintf(stderr, "%s could not be opened.\n", in_file_name);
+    if(!out) fprintf(stderr, "%s could not be opened.\n", out_file_name);
+
+    transpile_file(in, in_file_name, out, out_file_name);
+    return 0;
 }
+
+
+
+
+                                                                                                                                                                                                                                                                                                                             
