1c90c97ab6af4c79a6da303faddfc1b0540d2179
jame-parky <72101291+james-parky@users.noreply.github.com>
2022-07-22 17:47:26 +0100

    Added switch cases to ignore whitespace. Added check for non-ASCII chars and created a custom error handler for such scenario.

--- include/lexer.h
+++ include/lexer.h
@@ -5,12 +5,13 @@
 
 #include "token.h"
 
-#define LEX_BUF_MAX 1000
+#define LINE_BUF_MAX 1000
+#define TOK_BUF_MAX 1000
 
 typedef struct {
     char* file_name;
     FILE* file;
-    token* tok_buf[LEX_BUF_MAX];
+    token* tok_buf[TOK_BUF_MAX];
     size_t tok_buf_index;
     size_t tok_count;
 } lexer;
@@ -19,7 +20,7 @@ typedef struct {
  * lexer_init: Creates a new lexer struct, initialise the struct             *
  *             variables and returns a pointer to said lexer.                *
  *****************************************************************************/
-lexer* lexer_init(FILE* in_file, char* in_file_name);
+lexer* lexer_init (FILE* in_file, char* in_file_name);
 
 /*****************************************************************************
  * lexer_lex: Create tokens for each line in the given lexer struct's input  *
@@ -27,18 +28,23 @@ lexer* lexer_init(FILE* in_file, char* in_file_name);
  *            token buffer. Remove comment tokens from the buffer if the     *
  *            value of remove_comments is not zero.                          *
  *****************************************************************************/
-void lexer_lex(lexer* lex, int remove_comments);
+void lexer_lex (lexer* lex, int remove_comments);
 
 /*****************************************************************************
  * lexer_tok_dump: Print all tokens in the given lexer's token buffer to     *
  *                 stdout.                                                   *
  *****************************************************************************/
-void lexer_tok_dump(lexer* lex);
+void lexer_tok_dump (lexer* lex);
 
 /*****************************************************************************
  * lexer_reset_buf_index: Resets the token buffer index and sets the token   *
  *                        count for the given lexer struct.                  *
  *****************************************************************************/
-void lexer_reset_buf_index(lexer* lex);
+void lexer_reset_buf_index (lexer* lex);
+
+/*****************************************************************************
+ * lexer_free_all: Free all tokens stored within the provided lexer struct.  *
+ *****************************************************************************/
+void lexer_free (lexer* lex);
 
 #endif
--- src/lexer.c
+++ src/lexer.c
@@ -5,9 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "error.h"
 #include "token.h"
 
-lexer* lexer_init(FILE* file, char* file_name) {
+lexer* lexer_init (FILE* file, char* file_name) {
     lexer* lex = malloc(sizeof(lexer));
     lex->file_name = file_name;
     lex->file = file;
@@ -16,7 +17,7 @@ lexer* lexer_init(FILE* file, char* file_name) {
     return lex;
 }
 
-void lexer_add_tok(lexer* lex, token_type type, char* val, size_t line_num,
+void lexer_add_tok (lexer* lex, token_type type, char* val, size_t line_num,
                     size_t col_num) {
     token* tok = new_tok(type, val, line_num, col_num);
     lex->tok_buf[lex->tok_buf_index] = tok;
@@ -24,18 +25,18 @@ void lexer_add_tok(lexer* lex, token_type type, char* val, size_t line_num,
     lex->tok_count++;
 }
 
-int is_empty_line(char* buf) {
+int is_empty_line (char* buf) {
     while(*buf) if(!isspace(*buf++)) return 0;
     return 1;
 }
 
-size_t get_str_len(char* buf) {
+size_t get_str_len (char* buf) {
     size_t i = 0;
     while(buf[i] && buf[i] != '"') i++;
     return i;
 }
 
-char* get_comment(const char* buf) {
+char* get_comment (const char* buf) {
     size_t i = 0;
     while(buf[i] && buf[i] != '\n') i++;
     char* comment = malloc(sizeof(char) * i + 1);
@@ -44,29 +45,32 @@ char* get_comment(const char* buf) {
     return comment;
 }
 
-size_t get_identifier_len(char* buf) {
+size_t get_identifier_len (char* buf) {
     size_t i = 0;
     while(isalnum(buf[i]) || buf[i] == '_') i++;
     return i;
 }
 
-size_t get_num_len(char* buf) {
+size_t get_num_len (char* buf) {
     size_t i = 0;
     while(buf[i] && (isdigit(buf[i]) || isxdigit(buf[i]))) i++;
     return i;
 }
 
-char* substr(const char* buf, size_t len) {
+char* substr (const char* buf, size_t len) {
     char* substr = malloc(sizeof(char) * len + 1);
     strncpy(substr, buf, len);
     substr[len + 1] = '\0';
     return substr;
 }
 
-void lex_line(lexer* lex, char* buf, size_t line_num) {
+void lex_line (lexer* lex, char* buf, size_t line_num) {
     size_t col = 0;
     while(buf[col] && buf[col] != '\n') {
         switch(buf[col]){
+            case ' ': break;
+            case '\n': break;
+            case '\t': break;
             case '-': 
                 lexer_add_tok(lex, TOK_MINUS, "-", line_num, col); break; 
             case '+': 
@@ -84,7 +88,7 @@ void lex_line(lexer* lex, char* buf, size_t line_num) {
             case ',': 
                 lexer_add_tok(lex, TOK_COMMA, ",", line_num, col); break; 
             case '#': 
-                lexer_add_tok(lex, TOK_COMMENT, get_comment(buf + col), 
+                lexer_add_tok(lex, TOK_COMMENT, get_comment(buf + col),
                                 line_num, col); 
                 return;
             case '"': {
@@ -114,56 +118,61 @@ void lex_line(lexer* lex, char* buf, size_t line_num) {
                         free(hex_num);
                     } else {
                         size_t dec_num_len = get_num_len(buf + col);
-                        lexer_add_tok(lex, TOK_DEC_NUM,
-                                        substr(buf + col, dec_num_len),
-                                        line_num, col);
+                        char* dec_num = malloc(sizeof(char) * dec_num_len + 1);
+                        strncpy(dec_num, buf + col, dec_num_len);
+                        lexer_add_tok(lex, TOK_DEC_NUM, dec_num, line_num,
+                                        col);
                         col += dec_num_len - 1;
+                        free(dec_num);
                     }
-                }
+                } else throw_non_ascii_error(lex, line_num, col, (wchar_t)buf[col]);
         }
         col++;
     }
 }
 
-void remove_token(lexer* lex, size_t index) {
-    for(size_t i = index; i < lex->tok_count-1; i++)
-        lex->tok_buf[i] = lex->tok_buf[i + 1];
+void remove_token (lexer* lex, size_t index) {
+    for (; index < lex->tok_count - 1; index++)
+        lex->tok_buf[index] = lex->tok_buf[index + 1];
 }
 
-void remove_comment_toks(lexer* lex) {
-    for(size_t i = 0; i < lex->tok_count; i++) {
-        size_t rem_count = 0;
-        if(lex->tok_buf[i]->type == TOK_COMMENT) {
+void remove_comment_toks (lexer* lex) {
+    size_t rem_count = 0;
+    for (size_t i = 0; i < lex->tok_count; i++) {
+        if (lex->tok_buf[i]->type == TOK_COMMENT) {
             remove_token(lex, i);
             rem_count++;
         }
         lex->tok_count -= rem_count;
+        rem_count = 0;
     }
+    lexer_reset_buf_index(lex);
 }
 
-void lexer_lex(lexer* lex, int remove_comments) {
+void lexer_lex (lexer* lex, int remove_comments) {
     size_t line_num = 0;
-    char line_buf[LEX_BUF_MAX];
-    while(fgets(line_buf, LEX_BUF_MAX, lex->file) != NULL) {
-        if(!is_empty_line(line_buf)) lex_line(lex, line_buf, line_num);
+    char line_buf[LINE_BUF_MAX];
+    while (fgets(line_buf, LINE_BUF_MAX, lex->file) != NULL) {
+        if (!is_empty_line(line_buf)) lex_line(lex, line_buf, line_num);
         line_num++;
     }
+    lexer_add_tok(lex, TOK_EOF, "", line_num, 0);
     lexer_reset_buf_index(lex);
-    //lexer_tok_dump(lex);
-    if(remove_comments) remove_comment_toks(lex);
-    lexer_reset_buf_index(lex);
-    lexer_tok_dump(lex);
+    if (remove_comments) remove_comment_toks(lex);
 }
 
-void lexer_tok_dump(lexer* lex) {
-    for(size_t i = 0; i < lex->tok_count; i++)
-    print_tok(i, lex->tok_buf[i]);
+void lexer_tok_dump (lexer* lex) {
+    for (size_t i = 0; i < lex->tok_count; i++)
+        print_tok(i, lex->tok_buf[i]);
 }
 
-void lexer_reset_buf_index(lexer* lex) {
+void lexer_reset_buf_index (lexer* lex) {
     lex->tok_buf_index = 0;
     lex->tok_buf[lex->tok_count] = NULL;
 }
-void lexer_free_tok(lexer* lex){
-    for(size_t i=0;i<lex->tok_count;i++) free(lex->tok_buf[i]);
+void lexer_free (lexer* lex){
+    for (size_t i = 0; i < lex->tok_count; i++) {
+        free(lex->tok_buf[i]->val);
+        free(lex->tok_buf[i]);
+    }
 }
