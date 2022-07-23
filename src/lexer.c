4343aca7671d54b759d35458b3c075804e39ef37
jame-parky <72101291+james-parky@users.noreply.github.com>
2022-07-23 12:29:45 +0100

    Added comments to each function.

--- src/lexer.c
+++ src/lexer.c
@@ -8,6 +8,10 @@
 #include "error.h"
 #include "token.h"
 
+/*****************************************************************************
+ * lexer_init: Creates a new lexer struct, initialise the struct             *
+ *             variables and returns a pointer to said lexer.                *
+ *****************************************************************************/
 lexer* lexer_init (FILE* file, char* file_name) {
     lexer* lex = malloc(sizeof(lexer));
     lex->file_name = file_name;
@@ -17,6 +21,12 @@ lexer* lexer_init (FILE* file, char* file_name) {
     return lex;
 }
 
+/*****************************************************************************
+ * lexer_add_tok: Creates a new token* pointer using the provided arguments  *
+ *                by passing them to the new_tok() function. This token*     *
+ *                pointer is the inserted into the provided lexer, and said  *
+ *                lexer's buffer indexes are updated.                        *
+ *****************************************************************************/
 void lexer_add_tok (lexer* lex, token_type type, char* val, size_t line_num,
                     size_t col_num) {
     token* tok = new_tok(type, val, line_num, col_num);
@@ -25,17 +35,32 @@ void lexer_add_tok (lexer* lex, token_type type, char* val, size_t line_num,
     lex->tok_count++;
 }
 
+/*****************************************************************************
+ * is_empty_line: Returns 1 if the provided line buffer contains nothing but *
+ *                whitespace. Returns 0 if the provided line buffer has any  *
+ *                non-whitespace characters in it.                           *
+ *****************************************************************************/
 int is_empty_line (char* buf) {
     while(*buf) if(!isspace(*buf++)) return 0;
     return 1;
 }
 
+/*****************************************************************************
+ * get_str_len: Returns the length of the string literal whose               *
+ *              first character is the first character in the given          *
+ *              char buffer.                                                 *
+ *****************************************************************************/
 size_t get_str_len (char* buf) {
     size_t i = 0;
     while(buf[i] && buf[i] != '"') i++;
     return i;
 }
 
+/*****************************************************************************
+ * get_comment: Returns a string containing the value of the comment literal *
+ *              whose first character is the first character of the given    *
+ *              char buffer.                                                 *
+ *****************************************************************************/
 char* get_comment (const char* buf) {
     size_t i = 0;
     while(buf[i] && buf[i] != '\n') i++;
@@ -45,18 +70,30 @@ char* get_comment (const char* buf) {
     return comment;
 }
 
+/*****************************************************************************
+ * get_identifier_len: Returns the length of the identifier literal whose    *
+ *                     first character is the first character in the given   *
+ *                     char buffer.                                          *
+ *****************************************************************************/
 size_t get_identifier_len (char* buf) {
     size_t i = 0;
     while(isalnum(buf[i]) || buf[i] == '_') i++;
     return i;
 }
 
+/*****************************************************************************
+ * get_num_len: Returns the length of the number literal whose first         *
+ *              digit is the first character in the given char buffer.       *
+ *****************************************************************************/
 size_t get_num_len (char* buf) {
     size_t i = 0;
     while(buf[i] && (isdigit(buf[i]) || isxdigit(buf[i]))) i++;
     return i;
 }
 
+/*****************************************************************************
+ * substr: Return the first n characters of the provided char buffer.        *
+ *****************************************************************************/
 char* substr (const char* buf, size_t len) {
     char* substr = malloc(sizeof(char) * len + 1);
     strncpy(substr, buf, len);
@@ -64,6 +101,14 @@ char* substr (const char* buf, size_t len) {
     return substr;
 }
 
+
+/*****************************************************************************
+ * lex_line: Check each char in the given line buffer and create a token     *
+ *           based on its value. Multi-character vauled tokens, such as      *
+ *           comments and strings are also lexed correctly. When a valid     *
+ *           token is found, it is added to the given lexer's token buffer   *
+ *           using the lexer_add_tok() function.                             *   
+ *****************************************************************************/
 void lex_line (lexer* lex, char* buf, size_t line_num) {
     size_t col = 0;
     while(buf[col] && buf[col] != '\n') {
@@ -131,11 +176,21 @@ void lex_line (lexer* lex, char* buf, size_t line_num) {
     }
 }
 
+/*****************************************************************************
+ * remove_token: Remove the token at the specified index from the given      *
+ *               lexer's token buffer.                                       *
+ *****************************************************************************/
 void remove_token (lexer* lex, size_t index) {
-    for (; index < lex->tok_count - 1; index++)
-        lex->tok_buf[index] = lex->tok_buf[index + 1];
+    for (size_t i = index; i < lex->tok_count - 1; i++)
+        lex->tok_buf[i] = lex->tok_buf[i + 1];
 }
 
+/*****************************************************************************
+ * remove_comment_toks: Remove any tokens from the given lexer struct's      *
+ *                      token buffer, whose type is of TOK_COMMENT. Then     *
+ *                      reduce the lexer's tok_count variable by the number  *
+ *                      of tokens that were removed.                         *
+ *****************************************************************************/
 void remove_comment_toks (lexer* lex) {
     size_t rem_count = 0;
     for (size_t i = 0; i < lex->tok_count; i++) {
@@ -146,9 +201,14 @@ void remove_comment_toks (lexer* lex) {
         lex->tok_count -= rem_count;
         rem_count = 0;
     }
-    lexer_reset_buf_index(lex);
 }
 
+/*****************************************************************************
+ * lexer_lex: Create tokens for each line in the given lexer struct's input  *
+ *            source file. Store all lexed tokens in the given lexer's       *
+ *            token buffer. Remove comment tokens from the buffer if the     *
+ *            value of remove_comments is not zero.                          *
+ *****************************************************************************/
 void lexer_lex (lexer* lex, int remove_comments) {
     size_t line_num = 0;
     char line_buf[LINE_BUF_MAX];
@@ -159,17 +219,29 @@ void lexer_lex (lexer* lex, int remove_comments) {
     lexer_add_tok(lex, TOK_EOF, "", line_num, 0);
     lexer_reset_buf_index(lex);
     if (remove_comments) remove_comment_toks(lex);
+    lexer_reset_buf_index(lex);
 }
 
+/*****************************************************************************
+ * lexer_tok_dump: Print all tokens in the given lexer's token buffer to     *
+ *                 stdout.                                                   *
+ *****************************************************************************/
 void lexer_tok_dump (lexer* lex) {
     for (size_t i = 0; i < lex->tok_count; i++)
         print_tok(i, lex->tok_buf[i]);
 }
 
+/*****************************************************************************
+ * lexer_reset_buf_index: Resets the token buffer index for the given lexer  *
+ *****************************************************************************/
 void lexer_reset_buf_index (lexer* lex) {
     lex->tok_buf_index = 0;
     lex->tok_buf[lex->tok_count] = NULL;
 }
+
+/*****************************************************************************
+ * lexer_free_all: Free all tokens stored within the provided lexer struct.  *
+ *****************************************************************************/
 void lexer_free (lexer* lex){
     for (size_t i = 0; i < lex->tok_count; i++) {
         free(lex->tok_buf[i]->val);
